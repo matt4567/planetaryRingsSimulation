47cea6ab6db77c459de9fdc6099b63bc70172a24
matt4567 <matt.moore35@yahoo.co.uk>
2018-11-18 13:45:16 +0000

    fixed comments indentation

--- Main.py
+++ Main.py
@@ -40,7 +40,7 @@ def measureGap(results):
 
 def particleSetup():
     '''Setup particles ready to be simulated'''
-#   Setup the start and end positions as well as the gap between each particle. 
+    # Setup the start and end positions as well as the gap between each particle.
     mag = int(math.log10(earthMoonDistance))
     diff = 10 ** (mag - 1)
     begin = findBucket((earthMoonDistance / (2 ** (2 / 3))), diff)
@@ -58,14 +58,14 @@ def runSimulation(startPos, diff):
     num = 2
     # a is the timestep in seconds
     a = 100
-    #create the planetary system with a particle. Note we are called the particle a rocket
-	#here for historic reasons. (I didin't want to change my class names - i know its lazy).  	
+    # create the planetary system with a particle. Note we are called the particle a rocket
+	# here for historic reasons. (I didin't want to change my class names - i know its lazy).
     planet = BigRock(massPlanet, -massMoon, earthMoonDistance)
     moon = BigRock(massMoon, massPlanet, earthMoonDistance)
     rocket = Rocket(startPos, planet.posRock, 0, moon.posRock, 0, massPlanet, massMoon, False, earthMoonDistance)
 
-# 	save the positions of the planet and moon and rocket. 
-# 	Note: planets are referred to as earth here for historic reasons. 
+    # save the positions of the planet and moon and rocket.
+    # Note: planets are referred to as earth here for historic reasons.
     earthPosX = []
     earthPosY = []
     moonPosX = []
@@ -76,10 +76,10 @@ def runSimulation(startPos, diff):
     rocketPosX.append(rocket.X)
     rocketPosY.append(0)
 
-# 	boolean to check if particle is in final quarter of orbit. 
+    # boolean to check if particle is in final quarter of orbit.
     finalQuarter = False
     while True:
-#     	simulate motion of planet and moon
+        # simulate motion of planet and moon
         yEarth, xEarth = planet.move(a, massPlanet, massMoon)
         yMoon, xMoon = moon.move(a, massPlanet, massMoon)
 
@@ -88,12 +88,12 @@ def runSimulation(startPos, diff):
         earthPosY.append(yEarth)
         moonPosX.append(xMoon)
         moonPosY.append(yMoon)
-	
-# 		simulate motion of particle using runge kutta approximation. 
+
+        # simulate motion of particle using runge kutta approximation.
         x, y, v_x, v_y = rocket.rungeKutta(xEarth, yEarth, xMoon, yMoon,a, False)
 
-# 		if particle is too far away from initial position its on an unstable orbit and should be ignored. 
-# 		return out of while loop in this case.
+        # if particle is too far away from initial position its on an unstable orbit and should be ignored.
+        # return out of while loop in this case.
         distance = np.sqrt(x**2 + y**2)
         if (distance < rocket.Start - 3 * diff or distance > rocket.Start + 3 * diff):
             print "dropping"
@@ -102,14 +102,14 @@ def runSimulation(startPos, diff):
 
         rocketPosX.append(x)
         rocketPosY.append(y)
-#       check if rocket is in final quarter of circular orbit. 
+        # check if rocket is in final quarter of circular orbit.
         if x > 0 and y < 0:
             finalQuarter = True
 
-# 		if particle has completed desired number of orbits (i.e. num is zero) and is in the
-# 		final quarter of its orbit then add all of its previous positions as well as the 
-# 		previous positions of the planet and moon its orbiting around to an array and
-# 		return this for later analysis. 
+        # if particle has completed desired number of orbits (i.e. num is zero) and is in the
+        # final quarter of its orbit then add all of its previous positions as well as the
+        # previous positions of the planet and moon its orbiting around to an array and
+        # return this for later analysis.
         elif finalQuarter:
             if num == 0 and y>=0:
                 rocketPos.append([earthPosX[:], earthPosY[:],
@@ -117,30 +117,30 @@ def runSimulation(startPos, diff):
                              rocketPosX[:], rocketPosY[:]])
         
                 return x
-#           if its in its final quarter but has not completed the desired number of orbits
-# 			just carry on with the simulation and reduce the number of completed orbits by one.
+            # if its in its final quarter but has not completed the desired number of orbits
+            # just carry on with the simulation and reduce the number of completed orbits by one.
             elif num != 0:
                 finalQuarter = False
                 num -= 1
 
 def analyandPlot(finalRocketPos, initialRocketPos, gap, begin, stop, diff):
     '''plot the results of the simuation'''
-# 	define resolution for grouping of particles to plot number statistics. 
+    # define resolution for grouping of particles to plot number statistics.
     resolution = gap * 10
-# 	create buckets for grouping
+    # create buckets for grouping
     buckets = np.arange(begin - 1 * diff, stop + 1 * diff, resolution)
-# 	analysis the number statistics by looking at the number densities of particles before 
-# 	and after simulation.
+    # analysis the number statistics by looking at the number densities of particles before
+    # and after simulation.
     densities = np.zeros(shape=(len(buckets)))
     densitiesOrig = np.zeros(shape=(len(buckets)))
     for index, i in enumerate(finalRocketPos):
         if i != None:
-#         	add particle to bucket and change density array accordingly. 
+            # add particle to bucket and change density array accordingly.
             bucket = findBucket(i, resolution)
             indexItem = np.where(bucket == buckets)
             densities[indexItem] += 1
 
-# 		do the same thing with the original positions.
+        # do the same thing with the original positions.
         bucketOrig = findBucket(initialRocketPos[index], resolution)
         indexOrig = np.where(bucketOrig == buckets)
         densitiesOrig[indexOrig] += 1
